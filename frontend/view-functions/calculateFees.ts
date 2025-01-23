2a917b8c437675b2c0e4e2be98046e7a71a71b29
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-23 09:58:49 +0100

    added convert to human readable

--- frontend/view-functions/calculateFees.ts
+++ frontend/view-functions/calculateFees.ts
@@ -1,12 +1,15 @@
 import {AccountAddress, U64} from "@aptos-labs/ts-sdk";
 import { aptosClient } from "@/utils/aptosClient";
 import { MODULE_ADDRESS } from "@/constants";
+import { convertAmountFromHumanReadableToOnChain } from "@/utils/helpers";
 
-export const getCalulateFee = async({amount}: {amount: bigint}) =>{
+
+//Need to fetch the asset metadata to get the decimals before I can call this function with the amount
+export const getCalulateFee = async({amount,decimals}: {amount: number,decimals: number}) =>{
     const result = await aptosClient().view<[string]>({
         payload: {
             function: `${AccountAddress.from(MODULE_ADDRESS)}::paydrop::calculate_fees`,
-            functionArguments: [new U64(amount)]
+            functionArguments: [convertAmountFromHumanReadableToOnChain(amount,decimals)]
         }
     })
     return BigInt(result[0])
