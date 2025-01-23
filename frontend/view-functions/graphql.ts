c212e7a270f8e3c4b639877d68ad95a11f8515e2
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-23 09:58:28 +0100

    fetching from graphql api

---
+++ frontend/view-functions/graphql.ts
@@ -0,0 +1,47 @@
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+
+
+export type Fa_metadata = {
+    name: string,
+    decimals: number,
+    symbol: string,
+    token_standard: string,
+    valid: boolean
+
+}
+
+export async function query_fungible_asset_metadata(network: Network, address: string): Promise<Fa_metadata> {
+    const aptosConfig = new AptosConfig({ network });
+    const aptos = new Aptos(aptosConfig);
+
+
+    const query = `
+      query GetFungibleAssetInfo($addr: String) {
+        fungible_asset_metadata(
+           where: {creator_address: {_eq: \"${address}\"}}
+        ) {
+           name
+           decimals
+           symbol
+           token_standard
+      }
+    }
+    `
+
+    const fa_asset: any = await aptos.queryIndexer({
+        query: {
+            query
+        }
+    })
+
+
+    const result = {
+        name: fa_asset.fungible_asset_metadata[0].name,
+        decimals: fa_asset.fungible_asset_metadata[0].decimals,
+        symbol: fa_asset.fungible_asset_metadata[0].symbol,
+        token_standard: fa_asset.fungible_asset_metadata[0].token_standard,
+        valid: true
+    }
+
+    return result
+}
