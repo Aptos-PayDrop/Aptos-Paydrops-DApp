06f5c15f9abf567b331d5ae3f4fa3e4f7e3eb798
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-23 09:56:24 +0100

    updated file paths

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,8 @@
 import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 // Internal pages
-import { Mint } from "@/pages/Mint";
+import { ClaimPaydrops } from "@/pages/ClaimPaydrops";
 import { CreateDropTree } from "@/pages/CreateDropTree";
-import { MyFungibleAssets } from "@/pages/DropsHistory";
-import {Theme} from "@radix-ui/themes"
+import { MyHistory } from "@/pages/DropsHistory";
 function Layout() {
   return (
     <>
@@ -12,19 +11,7 @@ function Layout() {
   );
 }
 
-//TODO: I need a claim page, a deposit page and a deposit history page
 
-//I need the claim page to show your past claims also I guess
-
-//TODO: It should scan for claims
-
-//TODO: Claims page should have input for root and sponsor
-
-//TOOD: create fungible asset should be create droptree
-//It accepts a CSV import, parses the csv and renders a page if it's not too large
-//it should provide a UI to manually enter the address and amount parameters too
-//then it upload the merkle tree on IRYS and interacts with the contract
-//The deposit history is fetched from IRYS and displayed in a row
 
 const router = createBrowserRouter([
   {
@@ -32,26 +19,26 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Mint />,
+        element: <ClaimPaydrops />,
       },
       {
-        path: "create-asset",
+        path: "create-droptree",
         element: <CreateDropTree />,
       },
       {
-        path: "my-assets",
-        element: <MyFungibleAssets />,
+        path: "history",
+        element: <MyHistory />,
       },
     ],
   },
 ]);
 
-//TODO: splash screen on reload
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+
     </>
   );
 }
