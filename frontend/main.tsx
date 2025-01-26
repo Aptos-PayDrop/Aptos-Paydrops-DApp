af98cae4380ea15feec95ab5281be9f6936e522a
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-26 07:12:37 +0100

    refactored imports

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -4,11 +4,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "@/App.tsx";
+import App from "@/App";
 // Internal components
-import { Toaster } from "@/components/ui/toaster.tsx";
-import { TooltipProvider } from "@/components/ui/tooltip.tsx";
-import { WalletProvider } from "@/components/WalletProvider.tsx";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { WalletProvider } from "@/components/WalletProvider";
 import { WrongNetworkAlert } from "@/components/WrongNetworkAlert";
 
 const queryClient = new QueryClient();
