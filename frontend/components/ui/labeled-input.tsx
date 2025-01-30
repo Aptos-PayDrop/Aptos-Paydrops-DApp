75b047fd0c3c4d435a264a89cbd9154de80b2fef
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-30 19:59:29 +0100

    added labelled input button

--- frontend/components/ui/labeled-input.tsx
+++ frontend/components/ui/labeled-input.tsx
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FC } from "react";
+import { Button } from "./button";
 
 export const LabeledInput: FC<{
   label: string;
@@ -21,18 +22,61 @@ export const LabeledInput: FC<{
   id,
   type = "number",
 }) => {
-  return (
-    <div className="flex flex-col item-center space-y-4 mt-4">
-      <Label htmlFor={id} tooltip={tooltip}>
-        {label} {required ? "" : "(optional)"}
-      </Label>
-      <Input
-        disabled={disabled}
-        type={type}
-        id={id}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-col item-center space-y-4 mt-4">
+        <Label htmlFor={id} tooltip={tooltip}>
+          {label} {required ? "" : "(optional)"}
+        </Label>
+        <Input
+          disabled={disabled}
+          type={type}
+          id={id}
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    );
+  };
+
+
+export const LabeledInputWithButton: FC<{
+  label: string;
+  required?: boolean;
+  tooltip: string;
+  disabled?: boolean;
+  value?: number | string;
+  type?: "number" | "text";
+  id: string;
+  btnClick?: () => void,
+  btnText?: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}> = ({
+  label,
+  required,
+  tooltip,
+  disabled,
+  value,
+  onChange,
+  id,
+  type = "number",
+  btnClick,
+  btnText
+}) => {
+    return (
+      <div className="flex flex-col item-center space-y-4 mt-4">
+        <Label htmlFor={id} tooltip={tooltip}>
+          {label} {required ? "" : "(optional)"}
+        </Label>
+        <div className="flex flex-row ">
+          <Input
+            disabled={disabled}
+            type={type}
+            id={id}
+            value={value}
+            onChange={onChange}
+          />
+          <Button className="ml-2" onClick={btnClick}>{btnText}</Button>
+        </div>
+      </div>
+    );
+  };
