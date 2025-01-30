c578b0b436bcfa8b031166b297f3888cecb2d1d5
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-30 19:59:55 +0100

    cleanup, refactor

--- frontend/pages/ClaimPaydrops.tsx
+++ frontend/pages/ClaimPaydrops.tsx
@@ -1,25 +1,20 @@
 import { Header } from "@/components/Header";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useState } from "react";
 import { LabeledInput } from "@/components/ui/labeled-input";
-import { WarningAlert } from "@/components/ui/warning-alert";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { fetchMerkleTree, fetchTreeByRoot } from "@/utils/Irys";
-import { getTree, setTree } from "@/utils/browserCache";
-import { PayDropsSpinner, UploadSpinner } from "@/components/UploadSpinner";
+import { fetchTreeByRoot } from "@/utils/Irys";
+import { PayDropsSpinner } from "@/components/UploadSpinner";
 import { useToast } from "@/components/ui/use-toast";
-import { PayDropsTable } from "./Mint/components/PayDrops";
+
 
 export function ClaimPaydrops() {
 
   const [root, setRoot] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
   const [disableInput, setDisableInput] = useState(false);
-  const [droptree, setDropTree] = useState<any>({root: undefined});
+  const navigate = useNavigate();
 
-  // const { account } = useWallet();
   const { toast } = useToast();
 
 
@@ -48,22 +43,14 @@ export function ClaimPaydrops() {
       return;
     }
 
-
-    const cachedTree = await getTree(root);
-
-    if (cachedTree.success) {
-      setDropTree(cachedTree.data);
-      setIsLoading(false);
-      return;
-    }
-
+    setDisableInput(true);
 
     //If not then do the fetch...
     const searchForTree = await fetchTreeByRoot(root);
     if (!searchForTree.found) {
-
+      setDisableInput(false);
       if (searchForTree.error === "not found") {
-        //TODO: this means the request was successful but not found
+        // this means the request was successful but not found
         toast_default("Not found", "The searched identifier does not exist!")
         return;
       }
@@ -72,30 +59,11 @@ export function ClaimPaydrops() {
       return;
     }
 
-    toast_default("Please wait", "Paydrops found! Processing...")
-    setDisableInput(true);
 
-    //Disable the input and button while processing, this is needed because large trees take time
-    setIsLoading(true)
-    // fetch merkle tree now
-    const fetchedMerkleTree = await fetchMerkleTree(searchForTree.data.id).catch((err) => {
-      toast_error("Failed to fetch merkle tree")
-      setDisableInput(false);
-      console.log(err);
-      setIsLoading(false);
-    });
+    setTimeout(() => {
+      navigate(`/droptree/${root}`)
+    }, 1000)
 
-
-    const droptree = { ...fetchedMerkleTree, creatorAddress: searchForTree.data.address }
-
-    //Cache the merkle tree in the browser so I don't need to fetch it anymore
-    setTree(root, droptree);
-    //Navigate to the tree page 
-    setDropTree(droptree)
-    console.log(droptree)
-
-    setIsLoading(false);
-    setDisableInput(false);
   }
 
   return (
@@ -103,13 +71,6 @@ export function ClaimPaydrops() {
       <Header title="Claim Paydrops" />
       <div className="flex flex-col md:flex-row items-start justify-between px-4 py-2 gap-4 max-w-screen-xl mx-auto">
         <div className="w-full md:w-2/3 flex flex-col gap-y-4 order-2 md:order-1">
-          {/* {(!account) && (
-            <WarningAlert title={account ? "Wrong account connected" : "No account connected"}>
-              You need to connect your wallet interact with smart contracts
-            </WarningAlert>
-          )} */}
-
-          <PayDropsSpinner on={isLoading} />
 
           <Card>
             <CardHeader>
@@ -135,8 +96,6 @@ export function ClaimPaydrops() {
           </Card>
         </div>
       </div>
-
-      {droptree?.root !== undefined ? <PayDropsTable droptree={droptree} /> : null}
 
     </>
   );
