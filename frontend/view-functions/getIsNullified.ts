c5e69ac052ddaf743e082a20c36056490385f58f
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-30 20:01:45 +0100

    getter for isNullified

--- frontend/view-functions/getIsNullified.ts
+++ frontend/view-functions/getIsNullified.ts
@@ -1,4 +1,4 @@
-import {AccountAddress,U256} from "@aptos-labs/ts-sdk";
+import { AccountAddress, U256 } from "@aptos-labs/ts-sdk";
 import { aptosClient } from "@/utils/aptosClient";
 import { MODULE_ADDRESS } from "@/constants";
 
@@ -6,15 +6,31 @@ import { MODULE_ADDRESS } from "@/constants";
 type GetIsNullifiedArg = {
     sponsor: string,
     root: bigint,
-    recipient: string
+    recipient?: string
 }
 
-export const getIsNullified = async ({sponsor, root, recipient}: GetIsNullifiedArg) =>{
+export const getIsNullified = async ({ sponsor, root, recipient }: GetIsNullifiedArg) => {
     const result = await aptosClient().view<[boolean]>({
         payload: {
-           function: `${AccountAddress.from(MODULE_ADDRESS)}::paydrop::is_nullified`,
-           functionArguments: [sponsor,new U256(root),recipient]
+            function: `${AccountAddress.from(MODULE_ADDRESS)}::paydrop::is_nullified`,
+            functionArguments: [sponsor, new U256(root), recipient]
         }
     })
     return result[0];
+}
+
+type GetIsNullifiedBulkArg = {
+    sponsor: string,
+    root: bigint,
+    checkedAddresses: Array<string>
+}
+
+export const getIsNullifiedBulk = async ({ sponsor, root, checkedAddresses }: GetIsNullifiedBulkArg) => {
+    const result = await aptosClient().view<[Array<boolean>]>({
+        payload: {
+            function: `${AccountAddress.from(MODULE_ADDRESS)}::paydrop::is_nullified_bulk`,
+            functionArguments: [sponsor, new U256(root), checkedAddresses]
+        }
+    })
+    return result[0]
 }
