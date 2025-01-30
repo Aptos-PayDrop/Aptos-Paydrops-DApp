a252c91d11d1e98fb2b505e10707b8571aa68d50
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-01-30 20:00:49 +0100

    created details card

---
+++ frontend/pages/other/components/DetailsCard.tsx
@@ -0,0 +1,55 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Link } from "react-router-dom";
+
+export function DetailsCard(props: {
+    creatorAddress: string,
+    root: string,
+    depositedAmount: number,
+    faName: string,
+    showEnable: boolean,
+    amountLeft: number,
+    totalLeaves: string,
+    unusedLeaves: string,
+    assetExplorerLink: string,
+    existsOnChain: boolean,
+    amountToDepositIfNotExists: string
+}) {
+    return <Card>
+        <CardHeader>
+            <CardTitle>Details</CardTitle>
+            <CardDescription>See the Paydrop details and interact with the smart contracts</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <Link className={buttonVariants({ variant: "outline" })} target={"_blank"} to={props.assetExplorerLink}>Fungible Asset Link</Link>
+            <Table>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>Creator:</TableCell>
+                        <TableCell>{props.creatorAddress}</TableCell>
+                    </TableRow>
+                    {props.showEnable && props.existsOnChain ? <TableRow>
+                        <TableCell>Withdrawals are disabled</TableCell>
+                        <TableCell><Button>Enable</Button></TableCell>
+                    </TableRow> : null}
+
+                    <TableRow>
+                        <TableCell>Root:</TableCell>
+                        <TableCell>{props.root}</TableCell>
+                    </TableRow>
+                    {props.existsOnChain ?
+                        <TableRow>
+                            <TableCell>Value:</TableCell>
+                            <TableCell>Total Deposit: <strong>{props.depositedAmount}{" "}{props.faName}</strong>, Remaining value: <strong>{props.amountLeft}{" "}{props.faName}</strong>, Withdrawals left: <strong>{props.unusedLeaves}/{props.totalLeaves}</strong></TableCell>
+                        </TableRow> : null}
+
+                    {!props.existsOnChain ? <TableRow>
+                        <TableCell>Amount to deposit:</TableCell>
+                        <TableCell>{props.amountToDepositIfNotExists}</TableCell>
+                    </TableRow> : null}
+                </TableBody>
+            </Table>
+        </CardContent>
+    </Card>
+}
