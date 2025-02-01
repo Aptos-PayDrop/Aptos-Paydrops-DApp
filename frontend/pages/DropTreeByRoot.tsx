26b09f0f73157f9a5c669c189d4bf2aec5e423c9
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-01 19:14:43 +0100

    implemented refund,enable, claim

--- frontend/pages/DropTreeByRoot.tsx
+++ frontend/pages/DropTreeByRoot.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/Header";
 import { useToast } from "@/components/ui/use-toast";
 import { ActionAlert, WarningAlert } from "@/components/ui/warning-alert";
 import { PayDropsSpinner } from "@/components/UploadSpinner";
-import { fetchMerkleTree, fetchTreeByRoot } from "@/utils/Irys";
+import { fetchMerkleTree, fetchTreeByRoot, getIrysURLFromId } from "@/utils/Irys";
 import { query_fungible_asset_metadata } from "@/view-functions/graphql";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useEffect, useState } from "react";
@@ -10,34 +10,58 @@ import { useNavigate, useParams } from "react-router-dom"
 import { DetailsCard } from "./other/components/DetailsCard";
 import { PayDropsTable } from "./other/components/PayDrops";
 import { IS_PROD, NETWORK } from "@/constants";
-import { Network } from "@aptos-labs/ts-sdk";
+import { Account, AccountAddress, Network } from "@aptos-labs/ts-sdk";
 import { DropTreeDetails } from "@/view-functions/getDroptreeDetails";
 import { Button } from "@/components/ui/button";
 import { getIsNullified } from "@/view-functions/getIsNullified";
-import { convertAmountFromOnChainToHumanReadable } from "@/utils/helpers";
+import { convertAmountFromHumanReadableToOnChain, convertAmountFromOnChainToHumanReadable } from "@/utils/helpers";
+import { Spinner } from "@/components/ui/spinner";
+import { refundDroptree } from "@/entry-functions/refund_droptree";
+import { aptosClient } from "@/utils/aptosClient";
+import { enablePaydrop } from "@/entry-functions/enable_paydrop";
+import { claimDroptree } from "@/entry-functions/claim_paydrop";
+import { convertStringTreeLayersToBigint, encodeForCircuit, generateMerkleProof, getMerkleRootFromMerkleProof } from "@/crypto/merkletree";
+import { computeProof, convertProofToVector, generateCommitmentHash, getSnarkArtifactsBrowserPath, hashAddressForSnark } from "@/crypto/utils";
+import { newDroptree } from "@/entry-functions/new_droptree";
+import Decimal from "decimal.js"
+
+
+type DropTree = {
+    creatorAddress: string,
+    leaves: string[],
+    root: string,
+    tree: {
+        layers: string[][]
+    },
+    addresses: string[],
+    amounts: number[],
+    nonce: string,
+    decimals: number,
+    fungible_asset_address: string
+}
+
 
 //If the root doesn't exists on chain but it can fetch by root fron Irys, then It should offer to create that tree and anyone can make the deposit!!
 export function DropTreeByRoot() {
     const params = useParams();
     const navigate = useNavigate();
-    const { account, wallet } = useWallet();
+    const { account, signAndSubmitTransaction } = useWallet();
     const [dataInfo, setDataInfo] = useState({
         id: "",
         creatorAddress: "",
         tags: []
     })
 
-    const [droptree, setTree] = useState<any>({})
+    const [droptree, setTree] = useState<DropTree>({} as DropTree);
 
     const [notfound, setNotfound] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [nullifiedAddresses, setNullifiedAddresses] = useState<any>([]);
     const [amINullified, setAmINullified] = useState(true);
     const [treeContainsMyAddress, setTreeContainsMyAddress] = useState(false)
     const [dropTreeDetails, setDroptreeDetails] = useState<any>({});
     const [fungibleAssetDetails, setFungibleAssetDetails] = useState<any>({});
     const [existsOnChain, setExistsOnChain] = useState(false);
-
+    const [disableOnCreateBtn, setDisableOnCreateBtn] = useState(false);
 
     const { toast } = useToast();
 
@@ -107,17 +131,14 @@ export function DropTreeByRoot() {
 
 
                 const dropTreeOnChainDetails = await DropTreeDetails({ sponsor: creatorAddress, root: BigInt(root) }).catch(err => {
-                    toast_default("Not found", "Unable to fetch on-chain data. The Tree doesn't exist")
+                    toast_default("Not found", "Unable to fetch on-chain data. The deposit doesn't exist")
                 });
                 if (dropTreeOnChainDetails) {
                     setExistsOnChain(true);
                     setDroptreeDetails(dropTreeOnChainDetails);
-
                     const isNullified = await getIsNullified({ sponsor: creatorAddress, root: BigInt(root), recipient: account?.address });
                     setAmINullified(isNullified)
-
                 }
-
 
             } else {
                 //NOT FOUND     
@@ -134,9 +155,6 @@ export function DropTreeByRoot() {
         fetchIrysGraphQlData()
     }, [])
 
-    if (!account) {
-        console.log("no account connected")
-    }
 
     function containsMyAddress(myAddress: string, addresses: Array<string>) {
         return addresses.includes(myAddress);
@@ -164,6 +182,185 @@ export function DropTreeByRoot() {
         return `${totalDeposit}`
     }
 
+    async function onRefundClicked() {
+        if (params.tree) {
+            try {
+                const inputTransaction = refundDroptree({
+                    root: BigInt(params.tree)
+                });
+
+                const response = await signAndSubmitTransaction(inputTransaction);
+
+                // Wait for the transaction to be committed
+                const committedTransactionResponse = await aptosClient().waitForTransaction({
+                    transactionHash: response.hash
+                })
+
+                //Refresh the page
+                if (committedTransactionResponse.success) {
+                    window.location.reload();
+                }
+            } catch (err: any) {
+                toast_error(err.message)
+            }
+        }
+    }
+
+    function sumAmounts(amounts: number[]) {
+        let sum = new Decimal(0);
+        for (let i = 0; i < amounts.length; i++) {
+            sum = sum.add(new Decimal(amounts[i]));
+        }
+        return sum.toNumber();
+    }
+
+
+    async function onCreate() {
+        if (params.tree) {
+            try {
+                setDisableOnCreateBtn(true)
+                const total_deposit = sumAmounts(droptree.amounts);
+                const inputTransaction = newDroptree({
+                    root: BigInt(params.tree),
+                    fa_address: droptree.fungible_asset_address,
+                    total_deposit,
+                    total_deposit_decimals: droptree.decimals,
+                    total_leaves: droptree.leaves.length,
+                    enabled: true, // If you create it here it will be automatically enabled
+                    url: getIrysURLFromId(dataInfo.id)
+                });
+
+                const response = await signAndSubmitTransaction(inputTransaction);
+
+                const committedTransactionResponse = await aptosClient().waitForTransaction({
+                    transactionHash: response.hash
+                })
+                //Reload the page
+                if (committedTransactionResponse.success) {
+                    window.location.reload();
+                }
+            } catch (err: any) {
+                console.error(err.message);
+                toast_error("An error occurred while processing the transaction")
+            } finally {
+                setDisableOnCreateBtn(false);
+            }
+        }
+    }
+
+    async function onEnableClicked() {
+        if (params.tree) {
+            try {
+                const inputTransaction = enablePaydrop({ root: BigInt(params.tree) });
+                const response = await signAndSubmitTransaction(inputTransaction);
+
+                //Wait for the transaction to be committed
+                const committedTransactionResponse = await aptosClient().waitForTransaction({
+                    transactionHash: response.hash
+                })
+                if (committedTransactionResponse.success) {
+                    window.location.reload();
+                }
+            } catch (err: any) {
+                toast_error(err.message)
+            }
+        }
+    }
+
+    async function onClaimClicked() {
+        if (params.tree && account) {
+            try {
+                const address = account.address;
+                let amount = 0;
+                let commitment = 0n;
+                for (let i = 0; i < droptree.addresses.length; i++) {
+                    if (droptree.addresses[i] === address) {
+                        amount = droptree.amounts[i];
+                        commitment = BigInt(droptree.leaves[i]);
+                        break;
+                    }
+                }
+                const nonce = BigInt(droptree.nonce);
+
+                const bcsBytes = AccountAddress.from(address).bcsToBytes();
+                const hashedAddress = await hashAddressForSnark(bcsBytes);
+
+                const hashed_commitment = await generateCommitmentHash(hashedAddress, amount, nonce)
+
+                if (hashed_commitment !== commitment) {
+                    //An error occured, I didn't recover the correct commitment
+                    console.error("Unable to recover commitment")
+                    return
+                }
+
+
+                const convertedLayers = convertStringTreeLayersToBigint(droptree.tree.layers);
+
+                const bigintLeaves = droptree.leaves.map((leaf: string) => BigInt(leaf));
+
+                const merkleProof = await generateMerkleProof(commitment, bigintLeaves, convertedLayers.layers)
+
+                if (!merkleProof) {
+                    //ERR it returned null
+                    return;
+                }
+                const merkleRoot = await getMerkleRootFromMerkleProof(merkleProof);
+
+                if (BigInt(droptree.root) !== merkleRoot) {
+                    //   /?Error the root was not recovered, the merkle proof is invalid...
+                    console.error("Unable to create a valid merkle proof")
+                    return;
+                }
+                // compute the proof
+                const encodedProof = encodeForCircuit(merkleProof);
+
+
+                const { wasmFilePath, zkeyFilePath } = getSnarkArtifactsBrowserPath()
+
+                const { proof, publicSignals: _ } = await computeProof({
+                    pathElements: encodedProof.pathElements,
+                    pathIndices: encodedProof.pathIndices,
+                    publicInputs: {
+                        root: BigInt(droptree.root),
+                        address: hashedAddress,
+                        nonce,
+                        amount,
+                    },
+                    snarkArtifacts: {
+                        wasmFilePath,
+                        zkeyFilePath
+                    }
+                })
+
+                const proof_vector = convertProofToVector(proof);
+
+                const inputTransaction = claimDroptree({
+                    sponsor: address,
+                    root: BigInt(droptree.root),
+                    amount: amount,
+                    amount_decimals: droptree.decimals,
+                    nonce: BigInt(droptree.nonce),
+                    proof: proof_vector
+                });
+
+                const response = await signAndSubmitTransaction(inputTransaction);
+
+                const committedTransactionResponse = await aptosClient().waitForTransaction({
+                    transactionHash: response.hash
+                })
+
+                //Refresh the page on success
+                if (committedTransactionResponse.success) {
+                    window.location.reload()
+                }
+
+            } catch (err: any) {
+                console.error(err)
+                toast_error("An error occured while processing the transaction")
+            }
+        }
+    }
+
     return <>
         <Header title="PayDrops" />
         <div className="flex flex-col md:flex-row items-start justify-between px-4 py-2 gap-4 max-w-screen-xl mx-auto">
@@ -175,7 +372,7 @@ export function DropTreeByRoot() {
                 )}
                 {(!existsOnChain) && (
                     <WarningAlert title={account ? "Not found on-chain" : "Missing smart contract data"}>
-                        This droptree does not exists on chain {account?.address === dataInfo.creatorAddress ? <Button variant={"outline"}>Create it</Button> : undefined}
+                        This droptree does not exists on chain {account?.address === dataInfo.creatorAddress ? <Button onClick={onCreate} variant={"outline"}>Create it</Button> : undefined}
                     </WarningAlert>
                 )}
                 <PayDropsSpinner on={isLoading} />
@@ -193,12 +390,14 @@ export function DropTreeByRoot() {
                     unusedLeaves={dropTreeDetails.unused_leaves}
                     totalLeaves={dropTreeDetails.total_leaves}
                     assetExplorerLink={getExplorerLinkForFA(droptree.fungible_asset_address)}
+                    onEnableClicked={onEnableClicked}
                 ></DetailsCard>
                 <ShowRefundButton
                     creatorAddress={dataInfo.creatorAddress}
                     connectedAddress={account?.address}
                     deposit_left={dropTreeDetails.deposit_left}
                     enabled={dropTreeDetails.enabled}
+                    onRefundClicked={onRefundClicked}
                 ></ShowRefundButton>
                 <ShowClaim
                     fa_symbol="stx"
@@ -206,10 +405,20 @@ export function DropTreeByRoot() {
                     addressCanClaim={treeContainsMyAddress}
                     addressNullified={amINullified}
                     treeEnabled={dropTreeDetails.enabled}
+                    onClaimClicked={onClaimClicked}
                 ></ShowClaim>
             </div>
         </div>
-        {droptree?.root !== undefined ? <PayDropsTable droptree={droptree} nullifiedAddresses={nullifiedAddresses} /> : null}
+        {droptree?.root !== undefined ? <PayDropsTable droptree={droptree} /> :
+            <div className="flex flex-row justify-center">
+                <div className="flex flex-col justify-center">
+                    <div className="flex flex-row justify-center">
+                        <Spinner />
+                    </div>
+                    <p>Loading addresses. This may take a while</p>
+                </div>
+            </div>
+        }
     </>
 }
 
@@ -218,7 +427,8 @@ function ShowRefundButton(props: {
     creatorAddress: string,
     connectedAddress?: string,
     deposit_left: number,
-    enabled: boolean
+    enabled: boolean,
+    onRefundClicked: () => void
 }) {
 
     const canWithdraw = props.creatorAddress === props.connectedAddress && props.deposit_left > 0 && props.enabled
@@ -227,18 +437,20 @@ function ShowRefundButton(props: {
     if (canWithdraw) {
         return <ActionAlert title="Refund">
             <p>You are the creator of this PayDrop so you may refund the remaining value any time. All future withdrawals will be canceled.</p>
-            <Button variant={"green"}>Process Refund</Button>
+            <Button onClick={props.onRefundClicked} variant={"green"}>Process Refund</Button>
         </ActionAlert>
     }
     return <div></div>
 }
+
 
 function ShowClaim(props: {
     claimAmount: string,
     fa_symbol: string,
     addressCanClaim: boolean,
     addressNullified: boolean,
-    treeEnabled: boolean
+    treeEnabled: boolean,
+    onClaimClicked: () => void
 }) {
 
     if (!props.addressCanClaim) {
@@ -249,7 +461,7 @@ function ShowClaim(props: {
 
         return <ActionAlert title="Claim Paydrop">
             <p>You are eligible to claim your share of this deposit. You can withdraw {props.claimAmount} {props.fa_symbol}</p>
-            <Button variant={"green"}>Withdraw</Button>
+            <Button onClick={props.onClaimClicked} variant={"green"}>Withdraw</Button>
         </ActionAlert>
     } else if (props.addressNullified && props.addressCanClaim && props.treeEnabled) {
 
