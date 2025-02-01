efa9678651967314e96115be8d735e2fdcc12202
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-01 19:13:02 +0100

    added new env var constant

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -3,3 +3,4 @@ export const MODULE_ADDRESS = import.meta.env.VITE_MODULE_ADDRESS;
 export const IS_DEV = Boolean(import.meta.env.DEV);
 export const IS_PROD = Boolean(import.meta.env.PROD);
 export const APTOS_API_KEY = import.meta.env.VITE_APTOS_API_KEY;
+export const IS_CIRCUIT_FINALIZED = import.meta.env.CIRCUIT_FINALIZED;
