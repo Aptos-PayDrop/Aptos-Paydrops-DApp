b2f3adafa32c39a61c713e8de0d08ec64ea41c88
StrawberryChocolateFudge <hopeter@protonmail.com>
2025-02-04 16:02:21 +0100

    refactored cache keys

--- frontend/utils/browserCache.ts
+++ frontend/utils/browserCache.ts
@@ -8,7 +8,7 @@ import {
 
 export async function setTreeCache(network: string, id: string, treeData: any): Promise<{ success: boolean, error: string }> {
     try {
-        const key = `${network}-${id}`
+        const key = `${network}-${id}-tree`
 
         await set(key, treeData);
         return {
@@ -25,7 +25,7 @@ export async function setTreeCache(network: string, id: string, treeData: any): 
 
 export async function getTreeCache(network: string, id: string): Promise<{ success: boolean, data: any, error: string }> {
     try {
-        const key = `${network}-${id}`
+        const key = `${network}-${id}-tree`
 
         const res = await get(key);
 
@@ -63,7 +63,7 @@ export type GetResult<T> = {
 
 export async function setFungibleAssetMetadata(network: Network, address: string, faMetadata: Fa_metadata): Promise<SetResult> {
     try {
-        const key = `${network.toString()}-${address}`
+        const key = `${network.toString()}-${address}-assetmeta`
         await set(key, faMetadata)
         return {
             success: true,
@@ -80,7 +80,7 @@ export async function setFungibleAssetMetadata(network: Network, address: string
 
 export async function getFungibleAssetMetadata(network: Network, address: string): Promise<GetResult<Fa_metadata | {}>> {
     try {
-        const key = `${network.toString()}-${address}`
+        const key = `${network.toString()}-${address}-assetmeta`
         const res = await get(key);
 
         if (res) {
@@ -106,7 +106,7 @@ export async function getFungibleAssetMetadata(network: Network, address: string
 
 export async function setIrysTreeTxsByRootCache(network: string, root: string, node: any) {
     try {
-        const key = `${network}-${root}`
+        const key = `${network}-${root}-rootcache`
         await set(key, node)
         return {
             success: true,
@@ -122,7 +122,49 @@ export async function setIrysTreeTxsByRootCache(network: string, root: string, n
 
 export async function getIrysTreeTxsByRootCache(network: string, root: string) {
     try {
-        const key = `${network}-${root}`
+        const key = `${network}-${root}-rootcache`
+        const res = await get(key);
+
+        if (res) {
+            return {
+                success: true,
+                error: "",
+                data: res
+            }
+        }
+        return {
+            success: false,
+            error: "not found",
+            data: {}
+        }
+    } catch (err) {
+        return {
+            success: false,
+            error: "threw error",
+            data: {}
+        }
+    }
+}
+
+export async function setUploadSuccessful(network: string, root: string, data: any) {
+    try {
+        const key = `${network}-${root}-uploadSuccess`
+        await set(key, data)
+        return {
+            success: true,
+            error: ""
+        }
+    } catch (err: any) {
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+}
+
+export async function getUploadSuccessful(network: string, root: string) {
+    try {
+        const key = `${network}-${root}-uploadSuccess`
         const res = await get(key);
 
         if (res) {
